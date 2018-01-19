ae1af1b6cf227f465abe9c0a0df2bdc348b8e7ef
Krishna Pradeep <psrinivas@mobiware.com>
2018-01-19 16:57:11 +0530

    Work On account Type view controller screen designs

--- Postablur/LoginViewControllers/AccountType/PBFreeAccountCell.swift
+++ Postablur/LoginViewControllers/AccountType/PBFreeAccountCell.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 
+
 class PBFreeAccountCell: UITableViewCell {
 
     override func awakeFromNib() {
--- Postablur/LoginViewControllers/AccountType/PBPaidAccoutCell.swift
+++ Postablur/LoginViewControllers/AccountType/PBPaidAccoutCell.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
+protocol PBPaidAccoutCellDelegate {
+    
+    func pbnextBtnDidTap()
+    
+}
+
 class PBPaidAccoutCell: UITableViewCell {
+
+    var paidDelegate : PBPaidAccoutCellDelegate? = nil
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
 
+    @IBAction func nextBtnAction(_ sender: UIButton)
+    {
+        if let paidDelegate = self.paidDelegate
+        {
+            paidDelegate.pbnextBtnDidTap()
+            
+        }
+        
+    }
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
