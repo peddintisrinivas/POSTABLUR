c94159c5353a62cda15c68f5e6ee029834f578f2
Krishna Pradeep <psrinivas@mobiware.com>
2018-01-20 19:00:46 +0530

    Added Like,share and Dollar Limit Cells

---
+++ Postablur/LoginViewControllers/RevealSettingVC/PBDollarLimitCell.swift
@@ -0,0 +1,24 @@
+//
+//  PBDollarLimitCell.swift
+//  Postablur
+//
+//  Created by Srinivas Peddinti on 1/20/18.
+//  Copyright © 2018 MobiwareInc. All rights reserved.
+//
+
+import UIKit
+
+class PBDollarLimitCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
